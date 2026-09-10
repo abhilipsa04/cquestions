2e1a8771bc84c45220f5fa1164b4f87a32ebb5b6
Abhilipsa04 <mahuriabhilipsa@gmail.com>
2026-09-10 23:54:17 +0530

    Add code for CalcPerimeterOfRectangle

---
+++ CalcPerimeterOfRectangle.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    float width, length;
+    printf("enter width & length :");
+    scanf("%f %f", &width, &length);
+    printf("the parimeter of rectangle is %.2f", 2 * (width + length));
+    return 0;
+}
