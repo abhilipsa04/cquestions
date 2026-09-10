eca69f8cf7231b607cf2efa6542d96e9dc5387bf
Abhilipsa04 <mahuriabhilipsa@gmail.com>
2026-09-11 00:12:58 +0530

    Add code for CalcArethmaticOperations

---
+++ CalcArethmaticOperations.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 5, b = 2, c = 3;
+    printf("%d\n", 5 * 2 - 2 * 3);
+    printf("%d\n", 5 * 2 / 2 * 3);
+    printf("%d\n", 5 * (2 / 2) * 3);
+    printf("%d\n", 5 + 2 / 2 * 3);
+    return 0;
+}
