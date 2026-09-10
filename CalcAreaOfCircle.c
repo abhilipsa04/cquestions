7c3b6f8a3febc0832604d8301a6d128a2360ac71
Abhilipsa04 <mahuriabhilipsa@gmail.com>
2026-09-10 23:46:59 +0530

    Add code for  CalcAreaOfCircle

---
+++ CalcAreaOfCircle.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    float radious;
+    printf("enter radious :");
+    scanf("%f", &radious);
+    printf("the area of a circle is %f", 3.14 * radious * radious);
+    return 0;
+}
